a8491eede0be936416f02bc7962cb3629b47be59
Sarai Escobar <saraiee17@yahoo.com>
2022-12-08 13:21:59 -0600

    basic html format for pages

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,7 +5,29 @@ import {Link} from 'react-router-dom';
 function Login(){
     return(
         <div className='login'>
- 
+            <form className="login__form" >
+                <h1 className="login__title">Log in</h1>
+
+              
+                <label htmlFor='Email' >Email</label>
+                <input type="text" name="email" label="email" />
+            
+
+           
+                <label htmlFor='password' >Password</label>
+                <input type="password" name="password" label="Password" />
+           
+
+                <button className="login__button">
+                    Log in
+                </button>
+
+                {/* {error && <div className="login__message">Failed to login</div>} */}
+            </form>
+            <p>
+                Need an account? <Link to="/signup">Sign up</Link>
+            </p>
+
         </div>
     )
 }
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,7 +4,27 @@ import {Link} from 'react-router-dom';
 function SignUp(){
     return(
         <div className='signup'>
-   
+             <form className="signup" >
+                <h1 className="signup__title">Sign up</h1>
+
+                <label htmlFor='first_name' >First Name</label>
+                <input type="text" name="first_name" label="First name" />
+                <label htmlFor='last_name' >Last Name</label>
+                <input type="text" name="last_name" label="Last name" />
+                <label htmlFor='email' >Email</label>
+                <input type="text" name="email" label="Email" />
+                <label htmlFor='password' >Password</label>
+                <input type="password" name="password" label="Password" />
+
+                <button className="signup__button">Sign up</button>
+
+                {/* {success && <div className="signup__message">Signed up!</div>}
+                {error && <div className="signup__message">{error}</div>} */}
+            </form>
+            <p>
+                Have an account? <Link to="/login">Log in</Link>
+            </p> 
+
         </div>
     )
 }
