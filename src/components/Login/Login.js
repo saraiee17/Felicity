1541c4649fc0ffdb2c8a16034f50c89e0e8ee7fb
Sarai Escobar <saraiee17@yahoo.com>
2022-12-08 13:06:55 -0600

    prior to checkout for login/signup feature branch

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,28 +5,7 @@ import {Link} from 'react-router-dom';
 function Login(){
     return(
         <div className='login'>
-            <form className="login__form" >
-                <h1 className="login__title">Log in</h1>
-
-                <div>
-                <label htmlFor='username' >Username</label>
-                <input type="text" name="username" label="username" />
-                </div>
-                
-                <div>
-                <label htmlFor='password' >Password</label>
-                <input type="password" name="password" label="Password" />
-                </div>
-
-                <button className="login__button">
-                    Log in
-                </button>
-
-                {/* {error && <div className="login__message">Failed to login</div>} */}
-            </form>
-            <p>
-                Need an account? <Link to="/signup">Sign up</Link>
-            </p>
+ 
         </div>
     )
 }
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,22 +4,7 @@ import {Link} from 'react-router-dom';
 function SignUp(){
     return(
         <div className='signup'>
-               <form className="signup" >
-                <h1 className="signup__title">Sign up</h1>
-
-                <input type="text" name="first_name" label="First name" />
-                <input type="text" name="last_name" label="Last name" />
-                <input type="text" name="email" label="Email" />
-                <input type="password" name="password" label="Password" />
-
-                <button className="signup__button">Sign up</button>
-
-                {/* {success && <div className="signup__message">Signed up!</div>}
-                {error && <div className="signup__message">{error}</div>} */}
-            </form>
-            <p>
-                Have an account? <Link to="/login">Log in</Link>
-            </p> 
+   
         </div>
     )
 }
