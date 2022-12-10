04943c929c262e50c75be3293402ea2135058c72
Sarai Escobar <saraiee17@yahoo.com>
2022-12-09 23:12:14 -0600

    books

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -1,6 +1,8 @@
 import './Suggestions.scss';
-import {useParams} from 'react-router-dom';
-
+import {useParams,Link} from 'react-router-dom';
+import harrypotter from '../../assets/harrypotter.png';
+import kurt from '../../assets/kurt.png';
+import rings from '../../assets/rings.png';
 
 function Suggestions({data}){
 const params= useParams();
@@ -13,9 +15,30 @@ console.log(suggestion)
 
     return(
         <div className='suggestions'>
+            <Link to='/'><button>Home</button></Link>
             <h2 className='Suggestions__title'>Best thing to do is:</h2>
             <p>{suggestion}</p>
-            
+            {suggestion[0] === 'go for a walk' ? <p>1</p> : ""}
+            {suggestion[0] === "take a 15 minute power nap"? <p>2</p> : ""}
+            {suggestion[0] === "read a chapter of a good book"?
+            <div className='bookflex'>
+             <a href='https://hpread.scholastic.com/HP_Book1_Chapter_Excerpt.pdf'><img className='book' src={harrypotter} alt='harry potter and the sorcerers stone'/></a> 
+             <a href='https://antilogicalism.com/wp-content/uploads/2018/04/slaughterhouse-five.pdf'><img className='book' src={kurt} alt='kurt book'/></a> 
+             <a href='https://s3.amazonaws.com/scschoolfiles/112/j-r-r-tolkien-lord-of-the-rings-01-the-fellowship-of-the-ring-retail-pdf.pdf'><img className='book' src={rings} alt='kurt book'/></a> 
+             </div>
+             : ""}
+            {suggestion[0] === "take a social media break"? <p>4</p> : ""}
+            {suggestion[0] === "do somethng small that makes you happy"? <p>5</p> : ""}
+            {suggestion[0] === "Congrats!! Ponder why and enjoy the flowers."? <p>6</p> : ""}
+            {suggestion[0] === "write down your thoughts"? <p>7</p> : ""}
+            {suggestion[0] === "take 3 deep breaths and empty your mind/meditate"? <p>9</p> : ""}
+            {suggestion[0] === "smell lavendar"? <p>10</p> : ""}
+            {suggestion[0] === "reduce screentime"? <p>11</p> : ""}
+            {suggestion[0] === "do something nice for someone else"? <p>12</p> : ""}
+            {suggestion[0] === "Practice deep breathing"? <p>13</p> : ""}
+            {suggestion[0] === "Take 10 minutes to do something calming"? <p>14</p> : ""}
+            {suggestion[0] === "talk to a friend"? <p>15</p> : ""}
+
         </div>
     )
 }
