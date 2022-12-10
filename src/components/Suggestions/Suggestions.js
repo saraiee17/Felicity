055eb3ffdf2f30f9abeceaa21827d600e1b05e7a
Sarai Escobar <saraiee17@yahoo.com>
2022-12-09 21:17:00 -0600

    commit with pages name Capitalize and gitconfigcore.ignorecasefalse

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -9,12 +9,13 @@ const found= data.find(object => object.id === params.id);
 const array=found.inneremotions;
 const item = array.at(params.index);
 const suggestion= Object.values(item);
-
+console.log(suggestion)
 
     return(
         <div className='suggestions'>
             <h2 className='Suggestions__title'>Best thing to do is:</h2>
             <p>{suggestion}</p>
+            
         </div>
     )
 }
