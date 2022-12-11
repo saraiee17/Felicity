efbc32ee3a803f4490f927debf67561eebac7b1c
Sarai Escobar <saraiee17@yahoo.com>
2022-12-11 11:31:58 -0600

    debug multiple default export error

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,8 +4,12 @@ import {Link} from 'react-router-dom';
 function Nav(){
     return(
         <>
+            <span class="material-symbols-outlined arrow">arrow_back</span>
+            <Link to='/login'><span class="material-symbols-outlined">login</span></Link>
+            <Link to='/login'><span class="material-symbols-outlined">cottage</span></Link>
+            <Link to='/calendar'><span class="material-symbols-outlined">account_circle</span></Link>
         </>
     )
 }
 
-export default Nav;
+export default Nav; 
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -13,11 +13,9 @@ function Main(){
 
     return(
         <div className='main'>
-            <div className='flex'>
-                {/* <Link to='/login'><img  className='return'src={login2} alt='return arrow/shuvel'/></Link>
-                <Link to='/calendar'><button  className='userpage' alt='my page'>My Page</button></Link> */}
+            <div className='main__flex'>
+                <h2 className='main__title'>Felicity</h2>
             </div>
-            <h2 className='main__title'>Felicity</h2>
             <Link to='/wheel'><img className='logo' src={logo} alt='wheel'/></Link>
         </div>
     )
