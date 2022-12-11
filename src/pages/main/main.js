76ec81a65a5b4c407cc885efd45f5d0d6c5f42c4
Sarai Escobar <saraiee17@yahoo.com>
2022-12-10 23:42:25 -0600

    pre develop merge

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,4 +1,4 @@
-import './Main.scss';
+import './main.scss';
 import {Link,useNavigate} from 'react-router-dom';
 import logo from '../../assets/felicitylogo.png';
 import login2 from '../../assets/login2.png';
